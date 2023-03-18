7446a285261e14ad263238360d5eac1fa0abb652
pramod-sj <pramodsinghjantwal@gmail.com>
2023-03-18 12:34:11 +0530

    Add isInsetConsumed variable for checking if inset already provided to avoid re-consuming same inset

--- app/src/main/java/com/pramod/eyecare/framework/ui/utils/InsetExts.kt
+++ app/src/main/java/com/pramod/eyecare/framework/ui/utils/InsetExts.kt
@@ -3,26 +3,25 @@ package com.pramod.eyecare.framework.ui.utils
 import android.os.Build
 import android.view.View
 import android.view.ViewGroup
-import android.view.WindowInsets
 import androidx.annotation.Px
 import androidx.core.view.*
 
 fun View.doWithInset(insetCallback: (view: View, top: Int, bottom: Int) -> Unit) {
+    var isInsetConsumed = false
     setOnApplyWindowInsetsListener { view, windowInsets ->
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            windowInsets.getInsets(WindowInsetsCompat.Type.systemBars()).let {
-                insetCallback(this, it.top, it.bottom)
-            }
-        } else {
-            windowInsets.consumeSystemWindowInsets().let {
-                insetCallback(this, it.systemWindowInsetTop, it.systemWindowInsetBottom)
+        if (!isInsetConsumed) {
+            isInsetConsumed = true
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                windowInsets.getInsets(WindowInsetsCompat.Type.systemBars()).let {
+                    insetCallback(this, it.top, it.bottom)
+                }
+            } else {
+                windowInsets.consumeSystemWindowInsets().let {
+                    insetCallback(this, it.systemWindowInsetTop, it.systemWindowInsetBottom)
+                }
             }
         }
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            WindowInsets.CONSUMED
-        } else {
-            windowInsets
-        }
+        windowInsets
     }
 }
 
