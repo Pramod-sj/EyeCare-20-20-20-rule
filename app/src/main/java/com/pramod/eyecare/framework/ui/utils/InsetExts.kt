72e217ed5e257d83e7ef55aee90c0b23dddfb259
pramod-sj <pramodsinghjantwal@gmail.com>
2023-03-18 13:01:35 +0530

    Add requestApplyInsetsWhenAttached() for inset

--- app/src/main/java/com/pramod/eyecare/framework/ui/utils/InsetExts.kt
+++ app/src/main/java/com/pramod/eyecare/framework/ui/utils/InsetExts.kt
@@ -23,6 +23,25 @@ fun View.doWithInset(insetCallback: (view: View, top: Int, bottom: Int) -> Unit)
         }
         windowInsets
     }
+    requestApplyInsetsWhenAttached()
+}
+
+private fun View.requestApplyInsetsWhenAttached() {
+    if (isAttachedToWindow) {
+        // We're already attached, just request as normal
+        requestApplyInsets()
+    } else {
+        // We're not attached to the hierarchy, add a listener to
+        // request when we are
+        addOnAttachStateChangeListener(object : View.OnAttachStateChangeListener {
+            override fun onViewAttachedToWindow(v: View) {
+                v.removeOnAttachStateChangeListener(this)
+                v.requestApplyInsets()
+            }
+
+            override fun onViewDetachedFromWindow(v: View) = Unit
+        })
+    }
 }
 
 fun View.updateMargin(
